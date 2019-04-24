2718dfe767c0f2e39a05901181a376c91fdce3b1
Prince0fTime <prince0ftime.github@gmail.com>
2019-04-23 21:43:11 -0500

    Updates

---
+++ precache-manifest.8458cf29fcbf2752117bcd6360111902.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  },
+  {
+    "revision": "758b4f6f24bf713915cb",
+    "url": "/clicky-game/static/js/main.24c6ab1b.chunk.js"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/clicky-game/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "758b4f6f24bf713915cb",
+    "url": "/clicky-game/static/css/main.63074bb0.chunk.css"
+  },
+  {
+    "revision": "1d3d5309a43d2ce2ad84bc11cd7f2432",
+    "url": "/clicky-game/index.html"
+  }
+];
